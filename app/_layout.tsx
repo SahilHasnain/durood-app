4a084bf7796f5c7e718beaa1a5c7122415b7f80c
SAHIL HASNAIN <86091126+SahilHasnain@users.noreply.github.com>
2026-03-17 11:07:05 +0000

    fix: issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
+import { useEffect } from "react";
 import "../global.css";
 
 export default function RootLayout() {
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync("#000000");
+  }, []);
+
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="light" backgroundColor="#000000" />
       <Stack screenOptions={{ headerShown: false }} />
     </>
   );
