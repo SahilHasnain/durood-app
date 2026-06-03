b4650e76382ea40877cb0008a9ff38717df63aac
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-06-03 14:24:01 +0530

    Deploy Android versionCode 5

--- app.config.js
+++ app.config.js
@@ -47,7 +47,7 @@ export default {
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
       package: getUniqueIdentifier(),
-      versionCode: 4,
+      versionCode: 5,
     },
     web: {
       bundler: "metro",
